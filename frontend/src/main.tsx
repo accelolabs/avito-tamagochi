54340cc280674cc6d99d6818abf7a8508bc27dec
Sasha <st087955@student.spbu.ru>
2026-08-07 22:13:45 +0300

    Добавлен основной шрифт

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'normalize.css'
 import './index.css'
 import App from './App.tsx'
+import '@fontsource-variable/manrope';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
